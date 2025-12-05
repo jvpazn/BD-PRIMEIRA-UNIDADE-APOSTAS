d9081c6f09ba89443a184029d5bb39354280d378
JoaovPazn <joaovitor223344joao@gmail.com>
2025-12-05 10:19:23 -0300

    Insert multiple user records into usuario table

--- jackpot_if.sql
+++ jackpot_if.sql
@@ -55,3 +55,97 @@ CREATE TABLE usuario_grupo (
     FOREIGN KEY (grupo_id) REFERENCES grupo_comunidade(id),
     data_adesao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+IINSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Angela', 'OdeiovcAyin', 1200); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Roland', 'AngelicaMeuAmor', 75); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Malkuth', 'Malcute:3', 2100); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Yesod', 'Java>C++', 250); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Hod', 'ClubeLiteratura', 1800); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Netzach', 'BeberEapostar', 110000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Tiphereth', 'GarotasMagicas', 22); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Gebura', 'DISCIPLINA2000', 850); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Chesed', 'Cafe3coracoes', 78000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Binah', 'SouMelhorqueoAristoteles333', 9500); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Hokma', 'AmovcAyin', 120000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Mirinae', 'HANAassocDirector', 14500); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Harold', 'Debuff3x00', 450); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Olivier', 'AmovcRoland', 9500); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Pete', 'GuiaDosRatosparaviverNaCidade', 10); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Lenny', 'GuiaDosRatosparaviverNaCidade', 8); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Mang-Chi', 'GuiaDosRatosparaviverNaCidade', 15); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Ricardo', 'FAMILIAEM1LUGAR1!!!!!!!!!!!', 12000000000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Don Quixote', 'LosSangredeLosHermanos', 15000000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Sinclair', 'KromerKromerKromer1!!!!!!!!!!', 150000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Meursalt', 'Meursalt', 110000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Jia Qiu', 'DigaMeJIABAOYU,OqueHonguyanPrecisa?', 1300000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Zilu', 'AdeptoHeishou', 12000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Sancho', 'UmsonhoIrrealizavel', 1234); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Dulcinea', 'ArainhaDaParada', 11000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Nicolina', 'ManchalandComMascarasPraSempre', 2000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Curiambro', 'IreiMorrerPelosSeusPecados', 3000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Ahab', 'ACULPAÉTODASUAISHMAEL!', 100000000000000000000000000000000000000); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Queequeg', 'IshmaelTeAmo', 1500); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('StarBuck', 'StarbucksEmployee', 2500); 
+
+INSERT INTO usuario (nome_usuario, senha_hash, saldo_atual) 
+VALUES ('Ishmael', 'EUTEODEIOAHAB!!!!!!!!!!!!!!!!!!!!!!!!!!!', 3000); 
+
